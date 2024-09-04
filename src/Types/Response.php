bc3ea0a6276cb4ed0361c34b968008bcd2f513e3
Nikolai Shangin <shanginn@gmail.com>
2024-09-04 22:14:11 +0700

    Bump version to 2.2.0 and enhance the Response class constructor for improved clarity and usability.

--- src/Types/Response.php
+++ src/Types/Response.php
@@ -2,13 +2,28 @@
 
 namespace Phenogram\Bindings\Types;
 
+/**
+ * The response contains a JSON object, which always has a Boolean field 'ok' and may
+ * have an optional String field 'description' with a human-readable description
+ * of the result.
+ *
+ * If 'ok' equals True, the request was successful and the result of the query
+ * can be found in the 'result' field. In case of an unsuccessful request,
+ * 'ok' equals false and the error is explained in the 'description'.
+ *
+ * An Integer 'error_code' field is also returned, but its contents are subject to change in the future.
+ * Some errors may also have an optional field 'parameters' of the type ResponseParameters,
+ * which can help to automatically handle the error.
+ */
 class Response implements TypeInterface
 {
-    public bool $ok;
-
-    /** JSON encoded value of the result field */
-    public ?string $result = null;
-    public ?int $errorCode = null;
-    public ?string $description = null;
-    public ?ResponseParameters $parameters = null;
+    public function __construct(
+        public bool $ok,
+        /** JSON encoded value of the result field */
+        public ?string $result = null,
+        public ?int $errorCode = null,
+        public ?string $description = null,
+        public ?ResponseParameters $parameters = null,
+    ) {
+    }
 }
