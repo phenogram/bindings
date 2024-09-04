e45274ecf0e4bd71c939f9bc36ddc82955013260
Nikolai Shangin <shanginn@gmail.com>
2024-09-04 22:01:03 +0700

    Bump version to 2.1.1 in composer.json and add a comment for the result field in the Response class.

--- src/Types/Response.php
+++ src/Types/Response.php
@@ -5,6 +5,8 @@ namespace Phenogram\Bindings\Types;
 class Response implements TypeInterface
 {
     public bool $ok;
+
+    /** JSON encoded value of the result field */
     public ?string $result = null;
     public ?int $errorCode = null;
     public ?string $description = null;
